75cd3561390b1e02f4ea89145e3e890695d29b34
Nicholas Levenson <nicklevenson@gmail.com>
2020-11-24 15:47:40 -0800

    commit tree c6f2d619024efd603f7f2804b6705d5a786eef8d

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get "/newteam" do
+        erb :newteam
+    end
+
+    post "/team" do
+        puts params
+        @teamName = params[:name]
+        @coach = params[:coach]
+        @pointGaurd = params[:pg]
+        @shootingGaurd = params[:sg]
+        @smallForward = params[:sf]
+        @powerForward = params[:pf]
+        @center = params[:c]
+        erb :team
+    end
+
 
 end
